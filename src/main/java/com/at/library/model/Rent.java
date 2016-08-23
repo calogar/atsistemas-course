4cd4ca48aeb6fae33086400937fc238c0cf66894
Carlos Isidoro López García <carlosisidoro.lopez@gmail.com>
2016-08-23 20:33:19 +0200

    Create Rent Model

---
+++ src/main/java/com/at/library/model/Rent.java
@@ -0,0 +1,77 @@
+package com.at.library.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Rent implements Serializable {
+
+	private static final long serialVersionUID = 6374272004167410735L;
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Timestamp createdAt;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	private User user;
+
+	@OneToMany
+	private List<Book> books;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	private Employee employee;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+}
