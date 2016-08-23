aff665cf178332c8b350a2d776d9f4a6664c6c0c
Carlos Isidoro López García <carlosisidoro.lopez@gmail.com>
2016-08-23 19:32:01 +0200

    Update controller to use BookService to get all the Books

--- src/main/java/com/at/library/controller/BookController.java
+++ src/main/java/com/at/library/controller/BookController.java
@@ -3,20 +3,29 @@ package com.at.library.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.at.library.dto.BookDTO;
+import com.at.library.service.BookService;
 
 @RestController
 public class BookController {
 
+	@Autowired
+	private BookService bookservice;
+	
 	@RequestMapping(value = "/book", method = { RequestMethod.GET })
 	public List<BookDTO> getAll() {
+		return bookservice.findAll();
+		
+		/*
 		final BookDTO l1 = new BookDTO("111aaa", "El serñor de los anillos", "El tokien");
 		final BookDTO l2 = new BookDTO("111bbb", "La comunidad del anillo", "Tolkien");
 		return Arrays.asList(l1, l2);
+		*/
 	}
 
 }
