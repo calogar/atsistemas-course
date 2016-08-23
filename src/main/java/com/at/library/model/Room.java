83cc49559a60cdbb260d772f4d5c1122dda73c69
Carlos Isidoro López García <carlosisidoro.lopez@gmail.com>
2016-08-23 20:35:05 +0200

    Create Room Model

---
+++ src/main/java/com/at/library/model/Room.java
@@ -0,0 +1,46 @@
+package com.at.library.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+public class Room implements Serializable {
+	
+	private static final long serialVersionUID = 6374272004167410735L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String location;
+	
+	@OneToMany
+	private List<Shelf> shelves;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public List<Shelf> getShelves() {
+		return shelves;
+	}
+
+	public void setShelves(List<Shelf> shelves) {
+		this.shelves = shelves;
+	}
+}
