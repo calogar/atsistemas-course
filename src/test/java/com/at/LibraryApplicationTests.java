43510202511ae5546a8a41bf15a55142c9ec6ffb
Carlos Isidoro López García <carlosisidoro.lopez@gmail.com>
2016-08-24 14:47:54 +0200

    Example for testing POST create

--- src/test/java/com/at/LibraryApplicationTests.java
+++ src/test/java/com/at/LibraryApplicationTests.java
@@ -1,6 +1,7 @@
 package com.at;
 
 import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
 
 import com.at.library.dto.BookDTO;
 
@@ -10,8 +11,12 @@ public class LibraryApplicationTests {
 	public void contextLoads() {
 		// With RestTEmplate we can access every restful api
 		final String url = "http://localhost/8080/7";
-		final BookDTO book = (new RestTemplate).getForObject(url, BookDTO.class);
+		final BookDTO book = (new RestTemplate()).getForObject(url, BookDTO.class);
 		System.out.println(book);
+		
+		// If we wanted to test a create, we would use postForObject instead of create
+		// BookDTO book = new RestTemplate().postForObject(url, book, BookDTO.class)
+		
 	}
 
 }
