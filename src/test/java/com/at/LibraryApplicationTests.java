9bbafa8f5dea00bb9143153ebf998d92f67f930d
Carlos Isidoro López García <carlosisidoro.lopez@gmail.com>
2016-08-24 14:31:42 +0200

    Created basic Rest test for Book

--- src/test/java/com/at/LibraryApplicationTests.java
+++ src/test/java/com/at/LibraryApplicationTests.java
@@ -2,10 +2,16 @@ package com.at;
 
 import org.junit.Test;
 
+import com.at.library.dto.BookDTO;
+
 public class LibraryApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		// With RestTEmplate we can access every restful api
+		final String url = "http://localhost/8080/7";
+		final BookDTO book = (new RestTemplate).getForObject(url, BookDTO.class);
+		System.out.println(book);
 	}
 
 }
