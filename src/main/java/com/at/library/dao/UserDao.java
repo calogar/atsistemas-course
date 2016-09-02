9b08f1b86118c9e36ea116b2e6807d082b7a560e
Carlos Isidoro López García <carlosisidoro.lopez@gmail.com>
2016-09-02 13:03:15 +0200

    Adapt User Dao to pagination

--- src/main/java/com/at/library/dao/UserDao.java
+++ src/main/java/com/at/library/dao/UserDao.java
@@ -2,6 +2,7 @@ package com.at.library.dao;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -14,7 +15,7 @@ public interface UserDao extends CrudRepository<User, Integer> {
 	@Query(value = "select u from User as u where userStatus <> 'SUSPENDED'" + 
 				   "and (u.name  like %?1% or ?1 is null) " +
 				   "and (u.dni   like %?2% or ?2 is null) ")
-	public List<User> search(String name, String dni);
+	public List<User> search(String name, String dni, Pageable pageable);
 	
 	@Query(value = "select u from User as u where u.id = ?1 and userStatus <> 'SUSPENDED'")
 	public User findOne(Integer id);
