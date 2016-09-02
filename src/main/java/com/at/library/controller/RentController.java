90ac4b12ad3e3d63f464b04a6cbc0361e1591f0b
Carlos Isidoro López García <carlosisidoro.lopez@gmail.com>
2016-09-02 21:01:48 +0200

    Created Rent Controller

--- src/main/java/com/at/library/controller/RentController.java
+++ src/main/java/com/at/library/controller/RentController.java
@@ -14,8 +14,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.at.library.dto.RentDTO;
+import com.at.library.dto.RentHistoryDTO;
 import com.at.library.dto.RentPostDTO;
+import com.at.library.exceptions.BookNotAvailableException;
 import com.at.library.exceptions.BookNotFoundException;
+import com.at.library.exceptions.EmployeeNotFoundException;
+import com.at.library.exceptions.IdNotMatchingException;
+import com.at.library.exceptions.RentNotFoundException;
+import com.at.library.exceptions.UserNotFoundException;
+import com.at.library.service.RentService;
 
 @RestController
 public class RentController {
@@ -33,21 +40,25 @@ public class RentController {
 	}
 	
 	@RequestMapping(value = "book/{id}/rent", method = { RequestMethod.POST })
-	public RentDTO create(@PathVariable("id") Integer idBook,
-					  @RequestBody RentPostDTO rentPostDTO) {
+	public RentDTO create(@PathVariable("id") Integer idBook, @RequestBody RentPostDTO rentPostDTO)
+																throws IdNotMatchingException,
+	  															BookNotFoundException,
+	  															BookNotAvailableException,
+	  															UserNotFoundException,
+	  															EmployeeNotFoundException { 
 		log.debug(String.format("Renting this book: %s", idBook));
 		return rentService.create(idBook, rentPostDTO);
 	}
 	
 	@RequestMapping(value = "book/{id}/rent", method = { RequestMethod.DELETE })
-	public void delete(@PathVariable("id") Integer idBook) {
+	public void delete(@PathVariable("id") Integer idBook) throws BookNotFoundException, RentNotFoundException {
 		log.debug(String.format("Returning this book: %s", idBook));
 		rentService.delete(idBook);
 		
 	}
 
 	@RequestMapping(value = "book/{id}/rent", method = { RequestMethod.GET })
-	public List<RentDTO> getBookHistory(@PathVariable("id") Integer idBook,
+	public List<RentHistoryDTO> getBookHistory(@PathVariable("id") Integer idBook,
 									    @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
 									    @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) throws BookNotFoundException {
 		log.debug(String.format("History of the book: %s", idBook));
@@ -55,7 +66,7 @@ public class RentController {
 	}
 
 	@RequestMapping(value = "user/id/rent", method = { RequestMethod.GET })
-	public List<RentDTO> getUserHistory(@PathVariable Integer idUser,
+	public List<RentHistoryDTO> getUserHistory(@PathVariable Integer idUser,
 										@RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
 										@RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
 		log.debug(String.format("History of the user: %s", idUser));
