668a0acf4e81dcfb5cf34d53ad23093a30c21dfb
Carlos Isidoro López García <carlosisidoro.lopez@gmail.com>
2016-09-02 18:40:07 +0200

    Add transforms to interface

--- src/main/java/com/at/library/service/UserService.java
+++ src/main/java/com/at/library/service/UserService.java
@@ -6,34 +6,60 @@ import org.springframework.data.domain.Pageable;
 
 import com.at.library.dto.UserDTO;
 import com.at.library.exceptions.UserNotFoundException;
+import com.at.library.model.User;
 
 public interface UserService {
-	
+
 	/**
 	 * Creates a User and sets it's status to ACTIVE
-	 * @param UserDTO with input params
+	 * 
+	 * @param UserDTO
+	 *            with input params
 	 * @return Created UserDTO
 	 */
 	public UserDTO create(UserDTO userDTO);
 
 	/**
 	 * Sets the User status to SUSPENDED
-	 * @param The id of the User
-	 * @return 
+	 * 
+	 * @param The
+	 *            id of the User
+	 * @return
 	 */
 	public void delete(Integer id) throws UserNotFoundException;
-	
+
 	/**
 	 * Searchs Users by name, by dni or both
-	 * @param Name of the User
-	 * @param Dni of the User
+	 * 
+	 * @param Name
+	 *            of the User
+	 * @param Dni
+	 *            of the User
 	 */
 	public List<UserDTO> search(String name, String dni, Pageable pageable);
-	
+
 	/**
 	 * Get an User by it's id
+	 * 
 	 * @param Id of the user
 	 * @return The UserDTO
 	 */
 	public UserDTO findOne(Integer id) throws UserNotFoundException;
+
+	/**
+	 * Transforms a User into a UserDTO
+	 * 
+	 * @param User
+	 * @return UserDTO
+	 */
+	public UserDTO transform(User user);
+
+	/**
+	 * Transforms a UserDTO into a User
+	 * 
+	 * @param UserDTO
+	 * @return User
+	 */
+	public User transform(UserDTO userDTO);
+
 }
