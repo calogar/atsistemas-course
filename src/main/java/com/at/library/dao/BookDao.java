a96a47a1cc4ab882b91480f26ffc8dd60deac25b
Carlos Isidoro López García <carlosisidoro.lopez@gmail.com>
2016-09-02 12:58:14 +0200

    Adapt Book Dao to new statuses and pagination

--- src/main/java/com/at/library/dao/BookDao.java
+++ src/main/java/com/at/library/dao/BookDao.java
@@ -2,6 +2,7 @@ package com.at.library.dao;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,17 +13,19 @@ import com.at.library.model.Book;
 @Repository
 public interface BookDao extends CrudRepository<Book, Integer> {
 
+	/*
 	// Lo normal aquí es coger el modelo y devolver una lista de Libros, no de LibrosDTO. Pero si queremos evitar coger más datos de los que
 	// necesitamos (si la tabla tuviera 400 columns), podemos hacer una excepción y buscar un LibroDTO.
 	@Query(value = "select new com.at.library.dto.BookDTO(b.id, b.isbn, b.title, b.author) from Book as b where b.id in (select r.id from Rent as r where r.endAt is not null)")
 	public List<BookDTO> findUnavailable();
-
-	@Query(value = "select b from Book as b where status <> 'RENTED' " +
+	*/
+	
+	@Query(value = "select b from Book as b where status <> 'RENTED' and status <> 'DELETED' " +
 				   "and (b.title  like %?1% or ?1 is null) " +
 				   "and (b.isbn   like %?2% or ?2 is null) " + 
 				   "and (b.author like %?3% or ?3 is null)")
-	public List<Book> search(String title, String isbn, String author);
+	public List<Book> search(String title, String isbn, String author, Pageable pageable);
 
-	@Query(value = "select b from Book as b where b.id = ?1 and status <> 'RENTED' ")
+	@Query(value = "select b from Book as b where b.id = ?1 and status <> 'RENTED' and status <> 'DELETED'")
 	public Book findOne(Integer id);
 }
